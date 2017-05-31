474913641c0485d8459088bc372b90b705bb51e0
Matthew Vogee <mvogee@yahoo.com>
2017-05-31 11:44:31 -0700

    broke long string onto multiple lines

--- numbers/nPiDigits.py
+++ numbers/nPiDigits.py
@@ -1,6 +1,7 @@
 import math
 import sys
 
+# method 1 super easy but only holds to 15 points
 def nPiDigits(precision: int):
 	if (precision <= 15):
 		pi = round(math.pi, precision)
@@ -8,4 +9,15 @@ def nPiDigits(precision: int):
 	else:
 		print("maximum printable digits is 15")
 
-nPiDigits(int(sys.argv[1]))
+#method 2 up to 100 digits using string
+def nPiDigits2(precision: int):
+	pi = ("3.14159265358979323846"
+			"264338327950288419716939937510582097494"
+			"45923078164062862089986280348253421170679")
+	if (precision <= 100):
+		output = pi[0:precision + 2] # plus 2 for the 3.
+		print(output)
+	else:
+		print("maximum printable digits is 100")
+
+nPiDigits2(int(sys.argv[1]))
